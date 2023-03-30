b056cb745c9390494694dd2d565ab22edc48bf6f
HyunJun KIM <hihj070914@icloud.com>
2023-03-30 20:09:40 +0900

    add HW01_03

---
+++ assignment01/HW_01_03_draw_rectangles_and_ellipses.cpp
@@ -0,0 +1,127 @@
+/*
+** 2023 1st Semester : Computer Vision
+** HomeWork01-03 : HW_01_03_draw_rectangles_and_ellipses.cpp
+** written by HyunJun KIM (2019204054)
+** Summary : draw rectangles and ellipses while dragging, toggle by key event 
+** Comments beginning '##' are handwritten
+*/
+
+#include <iostream>
+using namespace std;
+
+#include <opencv2/opencv.hpp>
+using namespace cv;
+
+// Global variables
+Mat    g_imgColor;
+bool   g_isMousePressed = false;
+int    g_mouseStartX = -1;
+int    g_mouseStartY = -1;
+Scalar g_rectColor; // ## Rectangle's Color to draw
+int    g_prevMouseX; // ## record previous Mouse_X
+int    g_prevMouseY; // ## record previous Mouse_Y
+int    g_drawingMode; // ## toggle drawing shape. 0: rectangles, 1: ellipses
+
+// OpenCV Random Number Generator
+RNG g_rng(getTickCount());
+Scalar randomColor(RNG &g_rng)
+{
+    int icolor = (unsigned) g_rng;
+    return Scalar(icolor&255, (icolor>>8)&255, (icolor>>16)&255);
+}
+
+// Mouse callback function
+void mouse_callback(int event, int x, int y, int flags, void *param)
+{
+    // Left button pressed
+    if (event == EVENT_LBUTTONDOWN)
+    {
+        // Flag on
+        g_isMousePressed = true;
+        
+        // Record the mouse position
+        g_mouseStartX = x;
+        g_mouseStartY = y;
+
+		// ## Initialize the previous mouse position
+		g_prevMouseX = x;
+		g_prevMouseY = y;
+
+		// ## determine the color of rectangle
+		g_rectColor = randomColor(g_rng);
+    }
+    // Left button released
+    if (event == EVENT_LBUTTONUP)
+    {
+        // Flag off
+        g_isMousePressed = false;
+		switch (g_drawingMode)
+		{
+		case 0: // ## drawing Rectangles
+			rectangle(g_imgColor, Point(g_mouseStartX, g_mouseStartY), Point(x, y), g_rectColor, -1);
+			break;
+		case 1: // ## drawing Ellipses
+			ellipse(g_imgColor, RotatedRect(Point(g_mouseStartX, g_mouseStartY), \
+				Size(abs(x - g_mouseStartX), abs(y - g_mouseStartY)), 0), g_rectColor, -1);
+			break;
+		}
+    }
+	// ## Listen New Event : Mouse Cursor is moving on canvas 
+	if (event == EVENT_MOUSEMOVE)
+	{
+		if (g_isMousePressed) // ## Cursor is moving on canvas and Left Button is pressed
+		{
+			switch (g_drawingMode)
+			{
+			case 0: // ## drawing Rectangles
+				rectangle(g_imgColor, Point(g_mouseStartX, g_mouseStartY), Point(g_prevMouseX, g_prevMouseY), Scalar(0), -1);
+				rectangle(g_imgColor, Point(g_mouseStartX, g_mouseStartY), Point(x, y), g_rectColor, -1);
+				break;
+			case 1: // ## drawing Ellipses
+				ellipse(g_imgColor, RotatedRect(Point(g_mouseStartX, g_mouseStartY), \
+					Size(abs(g_prevMouseX - g_mouseStartX), abs(g_prevMouseY - g_mouseStartY)), 0), g_rectColor, -1);
+				ellipse(g_imgColor, RotatedRect(Point(g_mouseStartX, g_mouseStartY), \
+					Size(abs(x - g_mouseStartX), abs(y - g_mouseStartY)), 0), g_rectColor, -1);
+				break;
+		}
+			// ## Update the previous mouse position
+			g_prevMouseX = x;
+			g_prevMouseY = y;
+		}
+	}
+}
+
+int main()
+{
+    // Create a black image
+    const int rows = 480;
+    const int cols = 640;
+    g_imgColor = Mat::zeros(rows, cols, CV_8UC3);
+
+    // Create a window
+    String strWindowName = "Mouse Events";
+    namedWindow(strWindowName);
+
+    // Register the mouse callback function
+    setMouseCallback(strWindowName, mouse_callback);
+
+    // Infinite loop
+    while (true)
+    {
+        // Display the image
+        imshow(strWindowName, g_imgColor);
+
+        // Get user input
+        char key = waitKey(1);
+
+        // ESC
+        if (key == 27) break;
+		else if (key == 'm')
+			g_drawingMode = (g_drawingMode + 1) % 2 ;
+    }
+
+    // Destroy all windows
+    destroyAllWindows();
+
+    return 0;
+}
