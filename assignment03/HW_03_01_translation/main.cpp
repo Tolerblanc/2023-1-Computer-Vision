e8a9c416400b3a5e07f1ed48d63121368883b441
HyunJun KIM <hihj070914@icloud.com>
2023-05-24 00:03:07 +0900

    HW_03_01 translation

---
+++ assignment03/HW_03_01_translation/main.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+using namespace std;
+
+#include <opencv2/opencv.hpp>
+using namespace cv;
+
+// Global variables
+Mat		g_canvas;
+bool	g_isMousePressed = false;
+int		g_mouseStartX = -1;
+int		g_mouseStartY = -1;
+String	g_strWindowName = "translation";
+
+// Mouse callback function
+void mouse_callback(int event, int x, int y, int flags, void *param)
+{
+	// Left button pressed
+	if (event == EVENT_LBUTTONDOWN)
+	{
+		// Flag on
+		g_isMousePressed = true;
+
+		// Record the mouse position
+		g_mouseStartX = x;
+		g_mouseStartY = y;
+	}
+
+	// Left button released
+	if (event == EVENT_LBUTTONUP)
+	{
+		// Flag off
+		g_isMousePressed = false;
+	}
+
+	if (event == EVENT_MOUSEMOVE && g_isMousePressed)
+	{
+		Mat	M = (Mat_<double>(2,3) << 1, 0, x - g_mouseStartX,
+									  0, 1, y - g_mouseStartY); // translation 
+		warpAffine(g_canvas, g_canvas, M, g_canvas.size());
+		g_mouseStartX = x;
+		g_mouseStartY = y;
+	}
+}
+
+int main()
+{
+	// Create a window
+	namedWindow(g_strWindowName);	
+
+	g_canvas = imread("../messi5.jpg");
+
+	// Register the mouse callback function
+	setMouseCallback(g_strWindowName, mouse_callback);
+	// Infinite loop
+	while (true)
+	{
+		// Display the image
+		imshow(g_strWindowName, g_canvas);
+
+		// Get user input
+		char key = waitKey(1);
+
+		// ESC
+		if (key == 27) break;
+	}
+
+	// Destroy all windows
+	destroyAllWindows();
+
+	return 0;
+}
